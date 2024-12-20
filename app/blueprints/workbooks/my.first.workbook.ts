7130712e6bac22323ce49e1d050da9c88aeebe3d
Roby Hyde <hyde@mac.com>
2024-12-20 12:57:29 -0800

    Enhance workbook configuration and listener functionality for bids and lookups
    - Updated the workbook structure to include a new "Bids" sheet with fields for lot number, bid price, paddle number, and lookup fields for bidder, department, category, and tag.
    - Added a new listener in `listeners.ts` to handle bulk record processing, linking paddle numbers to bidders and codes to their respective department, category, and tag fields, enriching records with data from linked files.

--- app/blueprints/workbooks/my.first.workbook.ts
+++ app/blueprints/workbooks/my.first.workbook.ts
@@ -4,18 +4,113 @@ export const blueprint: Flatfile.CreateWorkbookConfig = {
   name: "My First Workbook",
   sheets: [
     {
-      name: "Contacts",
-      slug: "contacts",
+      name: "Bids",
+      slug: "bids",
       fields: [
         {
-          key: "name",
+          key: "lotNumber",
           type: "string",
-          label: "Name",
+          label: "Lot No.",
+        },
+        {
+          key: "bidPrice",
+          type: "number",
+          label: "Bid Price",
+        },
+        {
+          key: "paddleNumber",
+          type: "reference",
+          label: "Paddle No.",
+          config: {
+            ref: "bidders",
+            key: "paddleNumber",
+            relationship: "has-one",
+          },
+        },
+        {
+          key: "bidder",
+          type: "string",
+          label: "Bidder (lookup)",
+          readonly: true,
+        },
+        {
+          key: "code",
+          type: "reference",
+          label: "Code",
+          config: {
+            ref: "categories",
+            key: "code",
+            relationship: "has-one",
+          },
+
+        },
+        {
+          key: "department",
+          type: "string",
+          label: "Department (lookup)",
+          readonly: true,
+        },
+        {
+          key: "category",
+          type: "string",
+          label: "Category (lookup)",
+          readonly: true,
+        },
+        {
+          key: "tag",
+          type: "string",
+          label: "Tag (lookup)",
+          readonly: true,
+        },
+      ],
+    },
+    {
+      name: "Bidders",
+      slug: "bidders",
+      fields: [
+        {
+          key: "paddleNumber",
+          type: "string",
+          label: "Paddle No.",
+          constraints: [
+            { type: "unique" },
+            { type: "required" }
+          ],
         },
         {
           key: "email",
           type: "string",
           label: "Email",
+        },
+      ],
+    },
+    {
+      name: "Categories",
+      slug: "categories",
+      fields: [
+        {
+          key: "code",
+          type: "string",
+          label: "Code",
+          constraints: [
+            { type: "unique" },
+            { type: "required" }
+          ],
+        },
+        {
+          key: "department",
+          type: "string",
+          label: "Department",
+        },
+        {
+          key: "category",
+          type: "string",
+          label: "Category",
+        },
+        {
+          key: "tag",
+          type: "string",
+          label: "Tag",
         },
       ],
     },
--- app/listeners.ts
+++ app/listeners.ts
@@ -1,9 +1,62 @@
 import type { FlatfileListener } from "@flatfile/listener";
-
+import { FlatfileRecord, bulkRecordHook } from "@flatfile/plugin-record-hook";
 import { ExcelExtractor } from "@flatfile/plugin-xlsx-extractor";
 import spaceConfigure from "./jobs/space.configure";
 
 export default function (listener: FlatfileListener): void {
   listener.use(ExcelExtractor());
   listener.use(spaceConfigure);
+
+  listener.use(
+    bulkRecordHook("bids", async (records: FlatfileRecord[], event) => {
+
+      records.map((record) => {
+
+        if (record.get('paddleNumber')) {
+          const links = record.getLinks('paddleNumber')
+          const lookupValue = links?.[0]?.['email']
+          const targetField = 'bidder'
+          if (lookupValue !== undefined) {
+            record.set(targetField, lookupValue)
+            record.addInfo(targetField, 'From linked file')
+          }
+        }
+
+        if (record.get('code')) {
+          const links = record.getLinks('code')
+          const lookupValue = links?.[0]?.['department']
+          const targetField = 'department'
+          if (lookupValue !== undefined) {
+            record.set(targetField, lookupValue)
+            record.addInfo(targetField, 'From linked file')
+          }
+        }
+
+        if (record.get('code')) {
+          const links = record.getLinks('code')
+          const lookupValue = links?.[0]?.['category']
+          const targetField = 'category'
+          if (lookupValue !== undefined) {
+            record.set(targetField, lookupValue)
+            record.addInfo(targetField, 'From linked file')
+          }
+        }
+
+        if (record.get('code')) {
+          const links = record.getLinks('code')
+          const lookupValue = links?.[0]?.['tag']
+          const targetField = 'tag'
+          if (lookupValue !== undefined) {
+            record.set(targetField, lookupValue)
+            record.addInfo(targetField, 'From linked file')
+          }
+        }
+
+        return record;
+
+      })
+
+
+    })
+  )
 }
